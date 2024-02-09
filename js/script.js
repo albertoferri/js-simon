830b666ca304a057a01ea02871e86539b142b3d1
albertoferri <falberto99@gmail.com>
2024-02-09 16:54:47 +0100

    inizio esercizio creata funzione

---
+++ js/script.js
@@ -0,0 +1,8 @@
+// Sfruttiamo le timing functions per fare il conto alla rovescia per la correzione di lunedì!
+// Ogni secondo il nostro countdown dovrà scalare fino alle 9:30 di lunedì mattina!
+
+
+function Countdown() {
+    const now = new Date();
+    const monday = new Date();   
+}
